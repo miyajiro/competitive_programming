0e7e8f100970577cb691f126d6b99df802b0cfdb
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-04-14 00:40:30 +0900

    abc151e

---
+++ atcoder/abc151/e/e.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, v(T), greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+#define v(T) vector<T>
+#define vv(T) v(v(T))
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+using MOD = modint1000000007;
+
+ll N;
+ll K;
+vl A;
+MOD kaijo[100001];
+
+MOD comb(ll n, ll k){
+    return kaijo[n] * ((kaijo[k] * kaijo[n-k]).inv());
+}
+
+int main()
+{
+    cin >> N;
+    cin >> K;
+    rep(i, N){
+        ll a;
+        cin >> a;
+        A.pb(a);
+    }
+
+    MOD ans = 0;
+
+    kaijo[0] = 1;
+    rep1(i, 100000){
+        kaijo[i] = i * kaijo[i-1];
+    }
+
+    sort(rng(A));
+
+    rep(i, N - K + 1){ // 左端
+        ans -= comb(N - 1 - i, K - 1) * A[i];
+    }
+
+    for(int i = K - 1; i < N; i++){
+        ans += comb(i, K - 1) * A[i];
+    }
+
+    cout << ans.val() << endl;
+
+    return 0;
+}
