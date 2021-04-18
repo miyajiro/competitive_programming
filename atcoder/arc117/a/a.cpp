b63324732e51072f1c4fb3ada431b6a566455ffa
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-04-18 23:07:56 +0900

    arc117a

---
+++ atcoder/arc117/a/a.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, v(T), greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+#define v(T) vector<T>
+#define vv(T) v(v(T))
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int A, B;
+bool reversed = false;
+vl ansA, ansB;
+
+int main()
+{
+    // Aが正、Bが負
+    cin >> A >> B;
+    if(A < B){
+        swap(A, B);
+        reversed = true;
+    }
+
+    ll sum = 0;
+    rep1(i, A){
+        ansA.pb(i);
+        sum += i;
+    }
+
+    rep1(i, B-1){
+        ansB.pb(i);
+        sum -= i;
+    }
+
+    ansB.pb(sum);
+
+    if(!reversed){
+        cout << ansA[0];
+        rep1(i, ansA.size() - 1){
+            cout << " " << ansA[i];
+        }
+
+        rep(i, ansB.size()){
+            cout << " " << -ansB[i];
+        }
+        cout << endl;
+    } else {
+        cout << ansB[0];
+        rep1(i, ansB.size() - 1){
+            cout << " " << ansB[i];
+        }
+
+        rep(i, ansA.size()){
+            cout << " " << -ansA[i];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
