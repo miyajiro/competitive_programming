a5bcef7c5b5f682368108fc8a07e1f7048e2b09b
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-05-10 00:16:19 +0900

    arc118 a~c

---
+++ atcoder/arc118/c/c.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, v(T), greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+#define v(T) vector<T>
+#define vv(T) v(v(T))
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int N;
+vi A;
+
+int main()
+{
+    cin >> N;
+    A.pb(3 * 5 * 7 * 11);
+    rep1(i, 5000){
+        if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0){
+            A.pb(i * 2);
+        }
+    }
+
+    rep(i, N){
+        if(i == 0){
+            cout << A[i];
+            continue;
+        }
+        cout << " " << A[i];
+    }
+    newline;
+
+    return 0;
+}
