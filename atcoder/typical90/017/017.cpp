1f0165ce4a30ce342dd2dfa284b0140ebe5f3f02
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-05-15 03:00:00 +0900

    typical90-017

---
+++ atcoder/typical90/017/017.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, v(T), greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+#define v(T) vector<T>
+#define vv(T) v(v(T))
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll N, M;
+vl L, R, sortedR;
+v(LP) RAndId;
+vl Rs;
+vl r2Ids[300001];
+
+void solve()
+{
+    ll cnt = 0;
+    cin >> N >> M;
+    rep(i, M){
+        ll l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+        L.pb(l);
+        R.pb(r);
+        sortedR.pb(r);
+        Rs.pb(r);
+        r2Ids[r].pb(i);
+    }
+    sort(rng(sortedR));
+    sort(rng(Rs));
+    uni(Rs);
+
+    fenwick_tree<ll> ft(300001);
+
+    for(auto r : Rs){
+        for(auto id : r2Ids[r]){
+            ll l = L[id];
+            // cerr << "#" << l << "," << r << "\n";
+            // cerr << ft.sum(l, r + 1) << "\n\n";
+            cnt += ft.sum(l, r + 1);
+        }
+
+        ll rSize = r2Ids[r].size();
+        cnt += (rSize * (rSize - 1)) / 2LL;
+
+        for(auto id : r2Ids[r]){
+            ll l = L[id];
+            ft.add(l, 1LL);
+        }
+    }
+
+    // 完全に被らない場合もカウント
+    rep(i, M){
+        ll l = L[i];
+        cnt += (upper_bound(rng(sortedR), l) - sortedR.begin());
+    }
+
+    cout << (M * (M - 1) / 2LL) - cnt << "\n";
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
