097a4746bd75be250141b7f4f14e379cfaaf9bbd
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-05-16 15:55:43 +0900

    typical90-19,20

---
+++ atcoder/typical90/019/019.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, v(T), greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+#define v(T) vector<T>
+#define vv(T) v(v(T))
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const int inf = 0x3fffffff;
+int N;
+vi A;
+int dp[401][401];
+
+int calc(int l, int r){
+    if(dp[l][r] != -1){
+        return dp[l][r];
+    }
+    dp[l][r] = abs(A[r] - A[l]) + calc(l+1, r-1);
+    for(int i = l + 2; i <= r - 1; i+=2){
+        chmin(dp[l][r], calc(l, i - 1) + calc(i, r));
+    }
+    return dp[l][r];
+}
+
+void solve()
+{
+    cin >> N;
+    N *= 2;
+    rep(i, N){
+        int a;
+        cin >> a;
+        A.pb(a);
+    }
+
+    rep(i, N){
+        rep(j, N){
+            dp[i][j] = -1;
+        }
+    }
+
+    rep(i, N-1){
+        dp[i][i+1] = abs(A[i+1] - A[i]);
+    }
+
+    cout << calc(0, N-1) << endl;
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
