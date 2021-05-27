d8cc6b397b07fe73ba4e5600a82bbc7f787acd85
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-05-28 00:32:12 +0900

    update src by using library

--- aoj/DPL/DPL_5_C/main.cpp
+++ aoj/DPL/DPL_5_C/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <atcoder/modint>
 #define fr first
 #define sc second
 #define rep(i, n) for (int i = 0; i < (n); ++i)
@@ -23,6 +24,7 @@
 #define v(T) vector<T>
 #define vv(T) v(v(T))
 using namespace std;
+using namespace atcoder;
 using ll = long long;
 using uint = unsigned;
 using ull = unsigned long long;
@@ -60,111 +62,70 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
-// 素数 p で割ったあまりに関する構造体 (特に p = 1000000007 とすることが多い)
-template<int MOD> struct Fp {
-    long long val;
-    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
-        if (val < 0) v += MOD;
-    }
-    constexpr int getmod() { return MOD; }
-    constexpr Fp operator - () const noexcept {
-        return val ? MOD - val : 0;
-    }
-    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
-    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
-    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
-    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
-    constexpr Fp& operator += (const Fp& r) noexcept {
-        val += r.val;
-        if (val >= MOD) val -= MOD;
-        return *this;
-    }
-    constexpr Fp& operator -= (const Fp& r) noexcept {
-        val -= r.val;
-        if (val < 0) val += MOD;
-        return *this;
-    }
-    constexpr Fp& operator *= (const Fp& r) noexcept {
-        val = val * r.val % MOD;
-        return *this;
-    }
-    constexpr Fp& operator /= (const Fp& r) noexcept {
-        long long a = r.val, b = MOD, u = 1, v = 0;
-        while (b) {
-            long long t = a / b;
-            a -= t * b; swap(a, b);
-            u -= t * v; swap(u, v);
-        }
-        val = val * u % MOD;
-        if (val < 0) val += MOD;
-        return *this;
-    }
-    constexpr bool operator == (const Fp& r) const noexcept {
-        return this->val == r.val;
-    }
-    constexpr bool operator != (const Fp& r) const noexcept {
-        return this->val != r.val;
-    }
-    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
-        return os << x.val;
-    }
-    friend constexpr istream& operator >> (istream &is, Fp<MOD>& x) noexcept {
-        return is >> x.val;
-    }
-    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
-        if (n == 0) return 1;
-        auto t = modpow(a, n / 2);
-        t = t * t;
-        if (n & 1) t = t * a;
-        return t;
-    }
-};
+#ifndef TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include <atcoder/modint>
 
 // 二項係数ライブラリ
-template<class T> struct BiCoef {
-    vector<T> fact_, inv_, finv_;
-    constexpr BiCoef(int n) noexcept : fact_(n, 1), inv_(n, 1), finv_(n, 1) {
-        int MOD = fact_[0].getmod();
-        for(int i = 2; i < n; i++){
-            fact_[i] = fact_[i-1] * i;
-            inv_[i] = -inv_[MOD%i] * (MOD/i);
-            finv_[i] = finv_[i-1] * inv_[i];
+template <class mint, atcoder::internal::is_static_modint_t<mint> * = nullptr>
+struct BinomialCoefficient
+{
+    vector<mint> fact_, inv_, finv_;
+    constexpr BinomialCoefficient(int n) noexcept : fact_(n, 1), inv_(n, 1), finv_(n, 1)
+    {
+        int MOD = mint::mod();
+        for (int i = 2; i < n; i++)
+        {
+            fact_[i] = fact_[i - 1] * i;
+            inv_[i] = -inv_[MOD % i] * (MOD / i);
+            finv_[i] = finv_[i - 1] * inv_[i];
         }
     }
-    constexpr T com(int n, int k) const noexcept {
-        if (n < k || n < 0 || k < 0) return 0;
-        return fact_[n] * finv_[k] * finv_[n-k];
+    constexpr mint com(int n, int k) const noexcept
+    {
+        if (n < k || n < 0 || k < 0)
+            return 0;
+        return fact_[n] * finv_[k] * finv_[n - k];
     }
-    constexpr T fact(int n) const noexcept {
-        if (n < 0) return 0;
+    constexpr mint fact(int n) const noexcept
+    {
+        if (n < 0)
+            return 0;
         return fact_[n];
     }
-    constexpr T inv(int n) const noexcept {
-        if (n < 0) return 0;
+    constexpr mint inv(int n) const noexcept
+    {
+        if (n < 0)
+            return 0;
         return inv_[n];
     }
-    constexpr T finv(int n) const noexcept {
-        if (n < 0) return 0;
+    constexpr mint finv(int n) const noexcept
+    {
+        if (n < 0)
+            return 0;
         return finv_[n];
     }
 };
 
-using mint = Fp<1000000007>;
+using mint = modint1000000007;
 ll N, K;
 
 int main()
 {
     cin >> N >> K;
-    BiCoef<mint> bC(1010);
+    BinomialCoefficient<mint> bC(1010);
 
     mint ans = 0;
 
     for(ll i = 0; i <= K; i++){
-        mint d = ((K - i) % 2LL == 0) ? 1 : (ll)(ans.getmod() - 1);
-        ans += d * bC.com(K, i) * modpow((mint)(i), N);
+        mint d = ((K - i) % 2LL == 0) ? 1 : (ll)(mint::mod() - 1);
+        ans += d * bC.com(K, i) * ((mint)(i)).pow(N);
     }
 
-    cout << ans.val << endl;
+    cout << ans.val() << endl;
 
     return 0;
 }
