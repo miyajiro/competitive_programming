6c0fba5dad89a35e767b7113ccd23abb76014c10
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-05-31 22:49:39 +0900

    aoj 0401

---
+++ aoj/Volume4/0401/main.cpp
@@ -0,0 +1,124 @@
+#define TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+
+namespace atcoder{};
+using namespace atcoder;
+using namespace std;
+
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, v(T), greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int N;
+vi W, B;
+vvi grundyTable(301, vi(301, -1));
+
+int getGrundy(int w, int b){
+    if(grundyTable[w][b] != -1){
+        return grundyTable[w][b];
+    }
+    set<int> reachableGrundies;
+    if(w > 0){
+        reachableGrundies.insert(getGrundy(w - 1, b));
+    }
+    rep1(removeB, min(w, b)){
+        reachableGrundies.insert(getGrundy(w, b - removeB));
+    }
+    if(b > 0){
+        reachableGrundies.insert(getGrundy(w + 1, b - 1));
+    }
+    for(int g = 0;; g++){
+        if(reachableGrundies.find(g) == reachableGrundies.end()){
+            return grundyTable[w][b] = g;
+        }
+    }
+}
+
+void solve()
+{
+    cin >> N;
+    int g = 0;
+    grundyTable[0][0] = 0;
+    rep(i, N){
+        int w, b;
+        cin >> w >> b;
+        g ^= getGrundy(w, b);
+    }
+
+    cout << (g == 0 ? "1" : "0") << "\n";
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
