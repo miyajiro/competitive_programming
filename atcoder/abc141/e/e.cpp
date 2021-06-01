4292bafd613b4a0d2bcb8425ad9c0c019b8a33ee
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-06-01 15:05:26 +0900

    abc141e

--- atcoder/abc141/e/e.cpp
+++ atcoder/abc141/e/e.cpp
@@ -12,7 +12,6 @@
 // #include <atcoder/mincostflow>
 // #include <atcoder/scc>
 // #include <atcoder/twosat>
-
 namespace atcoder{};
 using namespace atcoder;
 using namespace std;
@@ -76,9 +75,123 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+#ifndef TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+template <unsigned mod>
+struct RollingHash
+{
+    vector<unsigned> hashed, power;
+
+    inline unsigned mul(unsigned a, unsigned b) const
+    {
+        unsigned long long x = (unsigned long long)a * b;
+        unsigned xh = (unsigned)(x >> 32), xl = (unsigned)x, d, m;
+        asm("divl %4; \n\t"
+            : "=a"(d), "=d"(m)
+            : "d"(xh), "a"(xl), "r"(mod));
+        return m;
+    }
+
+    RollingHash(const string &s, unsigned base = 10007)
+    {
+        int sz = (int)s.size();
+        hashed.assign(sz + 1, 0);
+        power.assign(sz + 1, 0);
+        power[0] = 1;
+        for (int i = 0; i < sz; i++)
+        {
+            power[i + 1] = mul(power[i], base);
+            hashed[i + 1] = mul(hashed[i], base) + s[i];
+            if (hashed[i + 1] >= mod)
+                hashed[i + 1] -= mod;
+        }
+    }
+
+    /**
+     * 区間[l, r) のハッシュ値を返す
+     * O(1)
+     */
+    unsigned get(int l, int r) const
+    {
+        unsigned ret = hashed[r] + mod - mul(hashed[l], power[r - l]);
+        if (ret >= mod)
+            ret -= mod;
+        return ret;
+    }
+
+    /**
+     * ハッシュ値h1と長さh2lenのハッシュ値h2を結合する
+     * O( log N )
+     */
+    unsigned connect(unsigned h1, int h2, int h2len) const
+    {
+        unsigned ret = mul(h1, power[h2len]) + h2;
+        if (ret >= mod)
+            ret -= mod;
+        return ret;
+    }
+
+    /**
+     * LCP (Longest Common Prefix)
+     * 区間[l1, r1)とハッシュテーブルがbからなる区間[l2, r2]の文字列の最長共通接頭辞の長さを求める
+     * O( log N )
+     */
+    int LCP(const RollingHash<mod> &b, int l1, int r1, int l2, int r2)
+    {
+        int len = min(r1 - l1, r2 - l2);
+        int low = -1, high = len + 1;
+        while (high - low > 1)
+        {
+            int mid = (low + high) / 2;
+            if (get(l1, l1 + mid) == b.get(l2, l2 + mid))
+                low = mid;
+            else
+                high = mid;
+        }
+        return (low);
+    }
+};
+
+using RH1 = RollingHash<1000000007>;
+
+int N;
+string S;
+int ans = 0;
+
+RH1 rh1("");
+
+int getLength(int l1, int l2){
+    int low = 0;
+    int high = l2 - l1 + 1;
+    int mid;
+    while(high - low > 1){
+        mid = (high + low) / 2;
+        if(rh1.get(l1, l1 + mid) == rh1.get(l2, l2 + mid) && S.substr(l1, mid) == S.substr(l2, mid)){
+            low = mid;
+        } else {
+            high = mid;
+        }
+
+        if(high <= ans){
+            return 0;
+        }
+    }
+    return low;
+}
+
 void solve()
 {
-    
+    cin >> N >> S;
+    rh1 = RH1(S);
+    rep(l1, N){
+        srep(l2, l1 + 1, N){
+            chmax(ans, getLength(l1, l2));
+        }
+    }
+    cout << ans << "\n";
 }
 
 int main()
