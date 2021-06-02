6a498a59e2be5ae790cec4845d0d7d5e62d3987f
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-06-02 14:55:50 +0900

    abc150f

--- atcoder/abc150/f/f.cpp
+++ atcoder/abc150/f/f.cpp
@@ -76,9 +76,141 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+#ifndef TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+template <unsigned mod>
+struct RollingHash
+{
+    vector<unsigned> hashed, power;
+
+    inline unsigned mul(unsigned a, unsigned b) const
+    {
+        unsigned long long x = (unsigned long long)a * b;
+        unsigned xh = (unsigned)(x >> 32), xl = (unsigned)x, d, m;
+        asm("divl %4; \n\t"
+            : "=a"(d), "=d"(m)
+            : "d"(xh), "a"(xl), "r"(mod));
+        return m;
+    }
+
+    RollingHash(const string &s, unsigned base = 2)
+    {
+        int sz = (int)s.size();
+        hashed.assign(sz + 1, 0);
+        power.assign(sz + 1, 0);
+        power[0] = 1;
+        for (int i = 0; i < sz; i++)
+        {
+            power[i + 1] = mul(power[i], base);
+            hashed[i + 1] = mul(hashed[i], base) + s[i];
+            if (hashed[i + 1] >= mod)
+                hashed[i + 1] -= mod;
+        }
+    }
+
+    /**
+     * 区間[l, r) のハッシュ値を返す
+     * O(1)
+     */
+    unsigned get(int l, int r) const
+    {
+        unsigned ret = hashed[r] + mod - mul(hashed[l], power[r - l]);
+        if (ret >= mod)
+            ret -= mod;
+        return ret;
+    }
+
+    /**
+     * ハッシュ値h1と長さh2lenのハッシュ値h2を結合する
+     * O( log N )
+     */
+    unsigned connect(unsigned h1, int h2, int h2len) const
+    {
+        unsigned ret = mul(h1, power[h2len]) + h2;
+        if (ret >= mod)
+            ret -= mod;
+        return ret;
+    }
+
+    /**
+     * LCP (Longest Common Prefix)
+     * 区間[l1, r1)とハッシュテーブルがbからなる区間[l2, r2]の文字列の最長共通接頭辞の長さを求める
+     * O( log N )
+     */
+    int LCP(const RollingHash<mod> &b, int l1, int r1, int l2, int r2)
+    {
+        int len = min(r1 - l1, r2 - l2);
+        int low = -1, high = len + 1;
+        while (high - low > 1)
+        {
+            int mid = (low + high) / 2;
+            if (get(l1, l1 + mid) == b.get(l2, l2 + mid))
+                low = mid;
+            else
+                high = mid;
+        }
+        return (low);
+    }
+};
+
+using RH = RollingHash<1000000007>;
+
+
+int N;
+const int M = 30;
+vi A, B;
+vector<string> sA(30), sB(30), sBInv(30);
+vector<RH> rhA(30, RH("")), rhB(30, RH("")), rhBInv(30, RH(""));
+
 void solve()
 {
-    
+    cin >> N;
+    rep(i, N){
+        int a;
+        cin >> a;
+        A.pb(a);
+        rep(m, M){
+            sA[m] += to_string((a >> m) & 1);
+        }
+    }
+    rep(i, N){
+        int b;
+        cin >> b;
+        B.pb(b);
+        rep(m, M){
+            sB[m] += to_string((b >> m) & 1);
+            sBInv[m] += to_string(1 - ((b >> m) & 1));
+        }
+    }
+
+    rep(m, M){
+        rhA[m] = RH(sA[m]);
+        rhB[m] = RH(sB[m]);
+        rhBInv[m] = RH(sBInv[m]);
+    }
+
+    rep(k, N){
+        int x = 0;
+        rep(m, M){
+            unsigned int hashA1 = rhA[m].get(k, N);
+            unsigned int hashA2 = rhA[m].get(0, k);
+            unsigned int hashA = rhA[m].connect(hashA1, hashA2, k);
+            unsigned int hashB = rhB[m].get(0, N);
+            unsigned int hashBInv = rhBInv[m].get(0, N);
+            if(hashA == hashBInv){
+                x |= 1 << m;
+            } else if(hashA != hashB) {
+                goto nextK;
+            }
+        }
+        cout << k << " " << x << "\n";
+
+        nextK:
+            ;
+    }
 }
 
 int main()
