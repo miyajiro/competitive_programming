63cc6c6c623b0a9c51da7a5d2c1b38186e5d47c7
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-06-03 22:47:40 +0900

    DSL_2_H

---
+++ aoj/DSL/DSL_2_H/main.cpp
@@ -0,0 +1,131 @@
+#define TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+#include <atcoder/lazysegtree>
+// #include <atcoder/string>
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+
+namespace atcoder{};
+using namespace atcoder;
+using namespace std;
+
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, vector<T>, greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = INT32_MAX;
+
+using S = ll;
+S op(S a, S b){
+    return min(a, b);
+}
+S e(){
+    return INF;
+}
+using F = ll;
+F mapping(F f, S x){
+    if(x == e()){
+        return x;
+    }
+    return f + x;
+}
+F composition(F f, F g){
+    return f + g;
+}
+F id(){
+    return 0;
+}
+
+int N, Q;
+
+
+void solve()
+{
+    cin >> N >> Q;
+    lazy_segtree<S, op, e, F, mapping, composition, id> seg(N);
+    rep(i, N){
+        seg.set(i, 0);
+    }
+    rep(i, Q){
+        int c, s, t, x;
+        cin >> c >> s >> t;
+        if(c == 0){
+            cin >> x;
+            seg.apply(s, t + 1, x);
+        } else {
+            cout << seg.prod(s, t + 1) << "\n";
+        }
+    }
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
