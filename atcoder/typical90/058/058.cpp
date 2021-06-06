3d52c7b5c64fc3c60cd6034a22f1650f1e131d08
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-06-07 01:00:11 +0900

    typical90 058

---
+++ atcoder/typical90/058/058.cpp
@@ -0,0 +1,125 @@
+#define TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+
+namespace atcoder{};
+using namespace atcoder;
+using namespace std;
+
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cout << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, vector<T>, greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int N;
+ll K;
+int loopCnt = 0;
+
+vector<bool> used(100000, false);
+int f(int x){
+    int y = x % 10 + x / 10 % 10 + x / 100 % 10 + x / 1000 % 10 + x / 10000;
+    return (x + y) % 100000;
+}
+
+void solve()
+{
+    cin >> N >> K;
+    if(K < 50000000){
+        rep(k, K){
+            N = f(N);
+        }
+        cout << N << "\n";
+        return;
+    }
+
+    int x = 100000;
+    rep(i, x){
+        N = f(N);
+    }
+    K -= x;
+    int cnt = 1;
+    int Y = f(N);
+    while(Y != N){
+        Y = f(Y);
+        cnt++;
+    }
+    K %= cnt;
+    rep(k, K){
+        N = f(N);
+    }
+    cout << N << "\n";
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
