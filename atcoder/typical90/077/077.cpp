dba6932fb1847754a9c9a9394a5e1404b481c6ab
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-07-08 23:49:40 +0900

    typical90 077

--- atcoder/typical90/077/077.cpp
+++ atcoder/typical90/077/077.cpp
@@ -8,7 +8,7 @@
 // #include <atcoder/convolution>
 // #include <atcoder/modint>
 // #include <atcoder/dsu>
-// #include <atcoder/maxflow>
+#include <atcoder/maxflow>
 // #include <atcoder/mincostflow>
 // #include <atcoder/scc>
 // #include <atcoder/twosat>
@@ -76,9 +76,88 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+int N;
+map<LP, int> mp;
+ll T;
+vl AX, AY, BX, BY;
+ll dx[8] = {1, 1, 0, -1LL, -1LL, -1LL, 0, 1};
+ll dy[8] = {0, 1, 1, 1, 0, -1LL, -1LL, -1LL};
+
 void solve()
 {
-    
+    vi from, to;
+    cin >> N >> T;
+
+    rep(i, 8){
+        dx[i] *= T;
+        dy[i] *= T;
+    }
+
+    rep(i, N){
+        ll ax, ay;
+        cin >> ax >> ay;
+        AX.pb(ax);
+        AY.pb(ay);
+    }
+
+    rep(i, N){
+        ll bx, by;
+        cin >> bx >> by;
+        BX.pb(bx);
+        BY.pb(by);
+        mp[LP(bx, by)] = i;
+    }
+
+    int fN = N * 2 + 2;
+    int s = fN - 2;
+    int t = fN - 1;
+    mf_graph<int> mf(fN);
+    rep(i, N){
+        mf.add_edge(s, i, 1);
+        mf.add_edge(N + i, t, 1);
+    }
+
+    rep(ni, N){
+        rep(di, 8){
+            int nx = AX[ni] + dx[di];
+            int ny = AY[ni] + dy[di];
+            auto ite = mp.find(LP(nx, ny));
+            if(ite != mp.end()){
+                int ni2 = ite->sc;
+                mf.add_edge(ni, ni2 + N, 1);
+            }
+        }
+    }
+
+    if(mf.flow(s, t) < N){
+        cout << "No\n";
+        return;
+    }
+
+
+    vi ans(N);
+
+    for(auto edge : mf.edges()){
+        if(edge.from == s || edge.to == t || edge.flow == 0){
+            continue;
+        }
+
+        int ni1 = edge.from;
+        int ni2 = edge.to - N;
+
+        ll DX = BX[ni2] - AX[ni1];
+        ll DY = BY[ni2] - AY[ni1];
+        rep(di, 8){
+            if(DX == dx[di] && DY == dy[di]){
+                ans[ni1] = di + 1;
+            }
+        }
+    }
+
+    cout << "Yes\n";
+    rep(i, N){
+        cout << ans[i] << (i != N - 1 ? " " : "\n");
+    }
 }
 
 int main()
