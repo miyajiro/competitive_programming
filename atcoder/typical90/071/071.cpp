198b0fc26435e452b1dc65c8a9b6efcbcfc6a311
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-08-08 20:31:51 +0900

    typical90 071

--- atcoder/typical90/071/071.cpp
+++ atcoder/typical90/071/071.cpp
@@ -76,9 +76,100 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+int N, M, K;
+vi A, B, D;
+vvi toArray;
+vvi ansList;
+vi now;
+
+int cnt = 0;
+
+vi usableNodes; // 現状入次数0の集合
+
+bool dfs(int i){ // i番目を埋める
+    if(cnt == K){
+        return false;
+    }
+
+    if(i == N){
+        ansList.eb(now);
+        cnt++;
+        return true;
+    }
+
+    if(usableNodes.empty()){
+        return false;
+    }
+
+    rrep(xi, sz(usableNodes)){
+        if(cnt == K){
+            return false;
+        }
+
+        int x = usableNodes[xi]; // i番目xi
+        usableNodes.erase(usableNodes.begin() + xi);
+        now[i] = x;
+
+        for(auto toNode : toArray[x]){
+            D[toNode]--;
+            if(D[toNode] == 0){
+                usableNodes.eb(toNode);
+            }
+        }
+
+        bool res = dfs(i + 1);
+
+        for(auto toNode : toArray[x]){
+            D[toNode]++;
+            if(D[toNode] == 1){
+                usableNodes.pop_back();
+            }
+        }
+
+        usableNodes.insert(usableNodes.begin() + xi, x);
+        now[i] = -1;
+
+        if(!res){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 void solve()
 {
-    
+    cin >> N >> M >> K;
+    toArray = vvi(N);
+    D = vi(N, 0);
+    now = vi(N, -1);
+
+    rep(i, M){
+        int a, b;
+        cin >> a >> b;
+        A.eb(--a);
+        B.eb(--b);
+        toArray[a].eb(b);
+        D[b]++;
+    }
+
+    rep(i, N){
+        if(D[i] == 0){
+            usableNodes.eb(i);
+        }
+    }
+
+    dfs(0);
+
+    if(cnt < K){
+        cout << -1 << "\n";
+    } else {
+        rep(k, K){
+            rep(n, N){
+                cout << ansList[k][n] + 1 << (n == N - 1 ? "\n" : " ");
+            }
+        }
+    }
 }
 
 int main()
