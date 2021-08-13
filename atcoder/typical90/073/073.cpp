ab51a602e0957af10e563f0124d6cf11950894f6
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-08-13 18:22:30 +0900

    typical90 073

--- atcoder/typical90/073/073.cpp
+++ atcoder/typical90/073/073.cpp
@@ -6,7 +6,7 @@
 // #include <atcoder/string>
 // #include <atcoder/math>
 // #include <atcoder/convolution>
-// #include <atcoder/modint>
+#include <atcoder/modint>
 // #include <atcoder/dsu>
 // #include <atcoder/maxflow>
 // #include <atcoder/mincostflow>
@@ -33,7 +33,7 @@ using namespace std;
 #define pcnt __builtin_popcountll
 #define uni(x) x.erase(unique(rng(x)), x.end())
 #define snuke srand((unsigned)clock() + (unsigned)time(NULL));
-#define show(x) cout << #x << " = " << x << endl;
+#define show(x) cerr << #x << " = " << x << endl;
 #define PQ(T) priority_queue<T, vector<T>, greater<T>>
 #define bn(x) ((1 << x) - 1)
 #define dup(x, y) (((x) + (y)-1) / (y))
@@ -76,9 +76,75 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+using mint = modint1000000007;
+
+int N;
+vvi G;
+vector<char> C;
+vector<vector<mint>> dp;
+
+vector<mint> dfs(int now, int from){
+    vector<mint> res(3, 0);
+    int A = 0;
+    int B = 1;
+    int AB = 2;
+
+    if(C[now] == 'b'){
+        swap(A, B);
+    }
+
+    // ここからはC[now] == 'a'とみなす
+    if(sz(G[now]) == 1 && G[now][0] == from){ // 自分が葉である場合
+        res[A] = 1;
+        res[B] = 0;
+        res[AB] = 0;
+        return dp[now] = res;
+    }
+
+    vector<vector<mint>> chValArray;
+    for(auto nex : G[now]){
+        if(nex == from){
+            continue;
+        }
+
+        chValArray.eb(dfs(nex, now));
+    }
+
+    mint aVal = 1;
+    mint abVal1 = 1;
+    mint abVal2 = 1;
+
+    for(auto chVal : chValArray){
+        aVal *= (chVal[A] + chVal[AB]);
+        abVal1 *= (chVal[A] + chVal[B] + 2 * chVal[AB]);
+        abVal2 *= (chVal[A] + chVal[AB]);
+    }
+
+    res[A] = aVal;
+    res[B] = 0;
+    res[AB] = abVal1 - abVal2;
+
+    return dp[now] = res;
+}
+
 void solve()
 {
-    
+    cin >> N;
+    G = vvi(N);
+    dp = vector<vector<mint>>(N, vector<mint>(3, 0));
+    rep(i, N){
+        string c;
+        cin >> c;
+        C.eb(c[0]);
+    }
+    rep(i, N - 1){
+        int x, y;
+        cin >> x >> y;
+        G[--x].eb(--y);
+        G[y].eb(x);
+    }
+
+    cout << dfs(0, -1)[2].val() << "\n";
 }
 
 int main()
