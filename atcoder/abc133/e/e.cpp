f0e59a97477c805d5aef0aca6af4fbc0bb82160e
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-09-04 16:24:28 +0900

    abc133 e

--- atcoder/abc133/e/e.cpp
+++ atcoder/abc133/e/e.cpp
@@ -6,7 +6,7 @@
 // #include <atcoder/string>
 // #include <atcoder/math>
 // #include <atcoder/convolution>
-// #include <atcoder/modint>
+#include <atcoder/modint>
 // #include <atcoder/dsu>
 // #include <atcoder/maxflow>
 // #include <atcoder/mincostflow>
@@ -41,13 +41,11 @@ using namespace std;
 using ll = long long;
 using uint = unsigned;
 using ull = unsigned long long;
-using P = pair<int, int>;
 using LP = pair<ll, ll>;
 using vi = vector<int>;
 using vvi = vector<vi>;
 using vl = vector<ll>;
 using vvl = vector<vl>;
-using vp = vector<P>;
 using vlp = vector<LP>;
 inline int getInt()
 {
@@ -76,9 +74,65 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+using mint = modint1000000007;
+int N, K;
+vi A, B;
+vvi G;
+vvi chArrayArray;
+
+mint kaijo[100010];
+
+mint P(int n, int k){
+    if(n < k){
+        return 0;
+    }
+    return kaijo[n] / kaijo[n - k];
+}
+
+void calcCh(int v, int prev){
+    for(auto nex : G[v]){
+        if(nex != prev){
+            chArrayArray[v].eb(nex);
+            calcCh(nex, v);
+        }
+    }
+}
+
+mint dfs(int v){ // vと、その1個下まで不定とした場合の通り数
+    vi chArray = chArrayArray[v];
+    if(sz(chArray) == 0){ // 1番下の場合
+        return 1;
+    }
+
+    mint res = 1;
+    for(auto ch : chArray){
+        res *= dfs(ch) * P(K - 2, sz(chArrayArray[ch]));
+    }
+
+    return res;
+}
+
 void solve()
 {
-    
+    cin >> N >> K;
+    G = vvi(N);
+    chArrayArray = vvi(N);
+    rep(i, N - 1){
+        int a, b;
+        cin >> a >> b;
+        A.eb(--a);
+        B.eb(--b);
+        G[a].eb(b);
+        G[b].eb(a);
+    }
+
+    kaijo[0] = 1;
+    rep1(i, 100009){
+        kaijo[i] = kaijo[i - 1] * i;
+    }
+    calcCh(0, -1);
+
+    cout << (dfs(0) * P(K, sz(G[0]) + 1)).val() << "\n";
 }
 
 int main()
