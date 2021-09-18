8c120175b58e98d940364140a2d8cd879245a8f5
Ryosuke Miyake <ryosuke12486@gmail.com>
2021-09-18 13:42:34 +0900

    abc157 d

---
+++ atcoder/abc157/d/d.cpp
@@ -0,0 +1,130 @@
+#define TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+#include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+
+namespace atcoder{};
+using namespace atcoder;
+using namespace std;
+
+#define fr first
+#define sc second
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (n); ++i)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define rrep1(i, n) for (int i = (n); i >= 1; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define isin(x, l, r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define uni(x) x.erase(unique(rng(x)), x.end())
+#define snuke srand((unsigned)clock() + (unsigned)time(NULL));
+#define show(x) cerr << #x << " = " << x << endl;
+#define PQ(T) priority_queue<T, vector<T>, greater<T>>
+#define bn(x) ((1 << x) - 1)
+#define dup(x, y) (((x) + (y)-1) / (y))
+#define newline puts("")
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vp = vector<P>;
+using vlp = vector<LP>;
+inline int getInt()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int N, M, K;
+vi A, B, C, D;
+vi ans;
+
+void solve()
+{
+    cin >> N >> M >> K;
+    dsu dSU(N);
+    ans = vi(N);
+    rep(m, M){
+        int a, b;
+        cin >> a >> b;
+        A.eb(--a);
+        B.eb(--b);
+        dSU.merge(a, b);
+    }
+    rep(k, K){
+        int c, d;
+        cin >> c >> d;
+        C.eb(--c);
+        D.eb(--d);
+    }
+
+    rep(i, N){
+        ans[i] = dSU.size(i) - 1;
+    }
+
+    rep(m, M){
+        ans[A[m]]--;
+        ans[B[m]]--;
+    }
+
+    rep(k, K){
+        if(dSU.same(C[k], D[k])){
+            ans[C[k]]--;
+            ans[D[k]]--;
+        }
+    }
+
+    rep(i, sz(ans)){
+        cout << ans[i] << (i == sz(ans) - 1 ? "\n" : " ");
+    }
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+    return 0;
+}
