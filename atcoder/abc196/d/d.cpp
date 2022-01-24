5fed9a24f6ce7821904f7204df7e2ad4df09e03b
miyajiro <ryosuke00719@gmail.com>
2022-01-24 21:52:12 +0900

    abc196 D

--- atcoder/abc196/d/d.cpp
+++ atcoder/abc196/d/d.cpp
@@ -76,9 +76,62 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+int H, W, A, B;
+
+int yx2N(int y, int x){
+    return y * W + x;
+}
+
+P n2YX(int n){
+    return P(n / W, n % W);
+}
+
+bool isUsed(int y, int x, int used){
+    int n = yx2N(y, x);
+    return (used >> n) % 2 == 1;
+}
+
+int use(int y, int x, int used){
+    int n = yx2N(y, x);
+    return used | (1 << n);
+}
+
+int dfs(int n, int nokori, int used){
+    P p = n2YX(n);
+    int y = p.fr;
+    int x = p.sc;
+
+    if(nokori == 0){
+        return 1;
+    }
+    if(y == H - 1 && x == W - 1 && nokori > 0){
+        return 0;
+    }
+
+    int res = 0;
+    // 横の場合
+    if(x < W - 1 && !isUsed(y, x, used) && !isUsed(y, x + 1, used)){
+        int newUsed = use(y, x, used);
+        newUsed = use(y, x + 1, used);
+        res += dfs(n + 1, nokori - 1, newUsed);
+    }
+
+    // 縦の場合
+    if(y < H - 1 && !isUsed(y, x, used) && !isUsed(y + 1, x, used)){
+        int newUsed = use(y, x, used);
+        newUsed = use(y + 1, x, used);
+        res += dfs(n + 1, nokori - 1, newUsed);
+    }
+
+    // 敷き詰めない場合
+    res += dfs(n + 1, nokori, used);
+    return res;
+}
+
 void solve()
 {
-    
+    cin >> H >> W >> A >> B;
+    cout << dfs(0, A, 0) << "\n";
 }
 
 int main()
