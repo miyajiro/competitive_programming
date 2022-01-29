d3a6e438efbecb2eff3e2ef4b15e4b73906a4e07
miyajiro <ryosuke00719@gmail.com>
2022-01-30 01:12:02 +0900

    abc173 E

--- atcoder/abc173/e/e.cpp
+++ atcoder/abc173/e/e.cpp
@@ -6,7 +6,7 @@
 // #include <atcoder/string>
 // #include <atcoder/math>
 // #include <atcoder/convolution>
-// #include <atcoder/modint>
+#include <atcoder/modint>
 // #include <atcoder/dsu>
 // #include <atcoder/maxflow>
 // #include <atcoder/mincostflow>
@@ -76,9 +76,88 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+int N, K;
+using mint = modint1000000007;
+using vm = vector<mint>;
+vl nA, pA;
+
 void solve()
 {
-    
+    cin >> N >> K;
+    rep(i, N){
+        ll a;
+        cin >> a;
+
+        if(a >= 0){
+            pA.eb(a);
+        } else {
+            nA.eb(-a);
+        }
+    }
+
+    sort(rrng(pA));
+    sort(rrng(nA));
+
+    mint ans = 1;
+    int pCnt = 0;
+    int pL = sz(pA);
+    int nCnt = 0;
+    int nL = sz(nA);
+
+    if(pL == 0 && K % 2 == 1){ // 絶対マイナス
+        sort(rng(nA));
+        rep(i, K){
+            ans *= -nA[i];
+        }
+        cout << ans.val() << "\n";
+        return;
+    }
+
+    while(pCnt + nCnt < K){
+        if(pCnt == pL){ // pを使い切った
+            ans *= -nA[nCnt++];
+            continue;
+        }
+
+        if(nCnt == nL){ // nを使い切った
+            ans *= pA[pCnt++];
+            continue;
+        }
+
+        if(nCnt == nL - 1){ // マイナスが残り1個
+            ans *= pA[pCnt++];
+            continue;
+        }
+
+        if(pCnt == pL - 1){ // プラスが残り1個
+            if((K - (pCnt + nCnt)) % 2 == 1){ // Kまで奇数ならプラスを入れる
+                ans *= pA[pCnt++];
+                continue;
+            } else { // Kまで偶数ならマイナス2つ
+                ans *= -nA[nCnt++];
+                ans *= -nA[nCnt++];
+                continue;
+            }
+            ans *= pA[pCnt++];
+            continue;
+        }
+
+        if(K - (pCnt + nCnt) == 1){ // 残り1個ならプラス
+            ans *= pA[pCnt++];
+            continue;
+        }
+
+        if(pA[pCnt] * pA[pCnt + 1] <= nA[nCnt] * nA[nCnt + 1]) {
+            ans *= -nA[nCnt++];
+            ans *= -nA[nCnt++];
+            continue;
+        } else {
+            ans *= pA[pCnt++];
+            continue;
+        }
+    }
+
+    cout << ans.val() << "\n";
 }
 
 int main()
