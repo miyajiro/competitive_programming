45c737dad2e12f1762a2b8d1b8c2bfd53abd3981
miyajiro <ryosuke00719@gmail.com>
2022-01-30 20:36:15 +0900

    abc114 D

--- atcoder/abc114/d/d.cpp
+++ atcoder/abc114/d/d.cpp
@@ -33,7 +33,7 @@ using namespace std;
 #define pcnt __builtin_popcountll
 #define uni(x) x.erase(unique(rng(x)), x.end())
 #define snuke srand((unsigned)clock() + (unsigned)time(NULL));
-#define show(x) cerr << #x << " = " << x << "\n";
+#define show(x) cerr << #x << " = " << x << endl;
 #define PQ(T) priority_queue<T, vector<T>, greater<T>>
 #define bn(x) ((1 << x) - 1)
 #define dup(x, y) (((x) + (y)-1) / (y))
@@ -76,9 +76,85 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+int N;
+vp primes;
+
 void solve()
 {
-    
+    cin >> N;
+
+    srep(i, 2, N + 1){
+        rep1(j, i - 1){
+            if(j > 1 && i % j == 0){
+                break;
+            }
+
+            if(j == i - 1){
+                int p = i;
+
+                int cnt = 0;
+                while(p <= N){
+                    cnt += N / p;
+                    p *= i;
+                }
+                primes.eb(P(i, cnt));
+                // show(primes.back().fr);
+                // show(primes.back().sc);
+            }
+        }
+    }
+
+    ll ans = 0;
+    // ^74
+    for(auto p : primes){
+        int k = p.sc;
+        if(k >= 74){
+            ans++;
+        }
+    }
+
+
+    for(auto p1 : primes){
+        for(auto p2 : primes){
+            if(p1 == p2){
+                continue;
+            }
+
+            int k1 = p1.sc;
+            int k2 = p2.sc;
+
+            // ^24 ^2
+            if(k1 >= 24 && k2 >= 2){
+                ans++;
+            }
+
+            // ^14 ^4
+            if(k1 >= 14 && k2 >= 4){
+                ans++;
+            }
+        }
+    }
+
+    // ^4 ^4 ^2
+    for(auto p1 : primes){
+        for(auto p2 : primes){
+            for(auto p3 : primes){
+                if(p1 >= p2 || p1 == p2 || p2 == p3 || p3 == p1){
+                    continue;
+                }
+
+                int k1 = p1.sc;
+                int k2 = p2.sc;
+                int k3 = p3.sc;
+
+                if(k1 >= 4 && k2 >= 4 && k3 >= 2){
+                    ans++;
+                }
+            }
+        }
+    }
+
+    cout << ans << "\n";
 }
 
 int main()
