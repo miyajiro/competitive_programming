0a9f25b4b1a0dd257827c53302b11a3e8aefdf66
miyajiro <ryosuke00719@gmail.com>
2022-02-21 16:37:15 +0900

    abc239 F wip

--- atcoder/abc239/f/f.cpp
+++ atcoder/abc239/f/f.cpp
@@ -76,9 +76,147 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+
+
+int N, M;
+vi D, A, B;
+
+vi par;
+vector<multiset<int>> st; // ここに欲しい人を格納。常に親を参照
+vector<P> ans; // a, b
+
+void dsuInit(){
+    par = vi(N);
+    st = vector<multiset<int>>(N);
+
+    rep(i, N){
+        par[i] = i;
+    }
+
+    rep(i, N){
+        rep(j, D[i]){
+            st[i].insert(i);
+        }
+    }
+}
+
+int getPar(int a){
+    if(par[a] == a){
+        return a;
+    }
+    return par[a] = getPar(par[a]);
+}
+
+void merge(int a, int b){
+    a = getPar(a);
+    b = getPar(b);
+
+    auto aReqIte = st[a].begin();
+    int aReq = *aReqIte;
+    st[a].erase(aReqIte);
+
+    auto bReqIte = st[b].begin();
+    int bReq = *bReqIte;
+    st[b].erase(bReqIte);
+
+    ans.eb(P(aReq, bReq));
+
+    par[b] = a;
+    st[a].merge(st[b]);
+
+    return;
+}
+
+int getReq(int a){ // その塊がどれくらい欲しているか
+    a = getPar(a);
+    return st[a].size();
+}
+
+bool isSame(int a, int b){
+    return getPar(a) == getPar(b);
+}
+
 void solve()
 {
-    
+    cin >> N >> M;
+
+    int dSum = 0;
+    rep(i, N){
+        int d;
+        cin >> d;
+        D.eb(d);
+        dSum += d;
+    }
+    if(dSum != 2 * N - 2){
+        cout << -1 << "\n";
+        return;
+    }
+
+    dsuInit();
+
+    rep(i, M){
+        int a, b;
+        cin >> a >> b;
+        --a;
+        --b;
+
+        if(getReq(a) <= 0 || getReq(b) <= 0){ // 元々つなげない場合はアウト
+            cout << -1 << "\n";
+            return;
+        }
+        merge(a, b);
+    }
+
+    // rep(i, N){
+    //     show(i);
+    //     show(getReq(i));
+    // }
+
+    // 連結成分がでかいやつから繋いでいく
+    vp reqAndPar;
+    rep(i, N){
+        // show(i);
+        // show(getPar(i));
+        if(i != getPar(i)){
+            continue;
+        }
+
+        reqAndPar.eb(P(getReq(i), i));
+    }
+    sort(rrng(reqAndPar));
+
+    {
+        // show(reqAndPar[0].fr);
+        // show(reqAndPar[0].sc);
+        int a = reqAndPar[0].sc;
+        rep1(pi, sz(reqAndPar) - 1){
+            // show(reqAndPar[pi].fr);
+            // show(reqAndPar[pi].sc);
+            int b = reqAndPar[pi].sc; // 受け継ぎ先の親
+
+            if(getReq(a) <= 0 || getReq(b) <= 0){ // 元々つなげない場合はアウト
+                cout << -1 << "\n";
+                return;
+            }
+            merge(a, b);
+        }
+    }
+
+    // 最後に0~N-1で連結してるか, 欲してるやついないか見る
+    rep(i, N){
+        if(getReq(i) > 0){
+            cout << -1 << "\n";
+            return;
+        }
+        if(!isSame(0, i)){
+            cout << -1 << "\n";
+            return;
+        }
+    }
+
+    srep(i, M, N - 1){
+        cout << ans[i].fr + 1 << " " << ans[i].sc + 1 << "\n";
+    }
 }
 
 int main()
