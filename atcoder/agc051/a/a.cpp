013d25d1af051b1473a9b7980d1eac1b6e3ce2fb
miyajiro <ryosuke00719@gmail.com>
2022-03-04 14:21:38 +0900

    agc 051 A

--- atcoder/agc051/a/a.cpp
+++ atcoder/agc051/a/a.cpp
@@ -58,7 +58,7 @@ using vb = vector<bool>;
 using vvb = vector<vb>;
 using vvvb = vector<vvb>;
 
-using mint = modint1000000007;
+using mint = modint998244353;
 using vm = vector<mint>;
 using vvm = vector<vm>;
 using vvvm = vector<vvm>;
@@ -90,9 +90,64 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
+int d;
+mint ans = 2;
+
+#ifndef TO_BE_SUBMITTED
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include <atcoder/modint>
+
+// 二項係数ライブラリ
+template <class mint, atcoder::internal::is_static_modint_t<mint> * = nullptr>
+struct BinomialCoefficient
+{
+    vector<mint> fact_, inv_, finv_;
+    constexpr BinomialCoefficient(int n) noexcept : fact_(n, 1), inv_(n, 1), finv_(n, 1)
+    {
+        int MOD = mint::mod();
+        for (int i = 2; i < n; i++)
+        {
+            fact_[i] = fact_[i - 1] * i;
+            inv_[i] = -inv_[MOD % i] * (MOD / i);
+            finv_[i] = finv_[i - 1] * inv_[i];
+        }
+    }
+    constexpr mint com(int n, int k) const noexcept
+    {
+        if (n < k || n < 0 || k < 0)
+            return 0;
+        return fact_[n] * finv_[k] * finv_[n - k];
+    }
+    constexpr mint fact(int n) const noexcept
+    {
+        if (n < 0)
+            return 0;
+        return fact_[n];
+    }
+    constexpr mint inv(int n) const noexcept
+    {
+        if (n < 0)
+            return 0;
+        return inv_[n];
+    }
+    constexpr mint finv(int n) const noexcept
+    {
+        if (n < 0)
+            return 0;
+        return finv_[n];
+    }
+};
+
+BinomialCoefficient<mint> bi(2000001);
+
 void solve()
 {
-    
+    cin >> d;
+    ans = bi.com(2 * d, d) * ((mint)(2)).inv();
+    cout << ans.val() << "\n";
 }
 
 int main()
